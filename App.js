0716bd67fa276a458832494b07ad386e130b2db5
Antony Ndambuki <143907986+Anto-4ny@users.noreply.github.com>
2025-02-03 12:04:04 +0000

    xz

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import WebDevelopmentPage from './WebDevelopmentPage';
-import ContactPage from './Contact'; // Make sure this exists
+import ContactPage from './Contact'; //Make sure this exists
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<WebDevelopmentPage />} />
+        <Route path="/web-development" element={<WebDevelopmentPage />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
