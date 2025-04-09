6746b471eafc5f4a4e70c2aac6331b6781a742ef
Antony Ndambuki <143907986+Anto-4ny@users.noreply.github.com>
2025-04-09 16:38:18 +0300

    Add files via upload

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,22 @@
+// src/plugins/vuetify.js
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components, 
+  directives,
+  theme: {
+    defaultTheme: 'light',
+  },
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: { mdi }
+  }
+})
+
+export default vuetify
